58370566c4b4b0320319d320f040e026586d5b17
nuo18 <119784955+nuo18@users.noreply.github.com>
2023-03-19 19:00:21 +0700

    added toss

--- main.py
+++ main.py
@@ -3,25 +3,53 @@ from randomize import *
 batsman = 0
 bowler = 0
 isOut = False
+isBatting = False
 total1 = 0
 total2 = 0
+choice = 0
 
-print("You are batting: ")
-while(isOut!=True):
-    print("Enter: ")
-    batsman = int(input())
-    bowler = bowl()
-    
-    if(batsman == bowler):
-        print("You are out!")
-        print("You scored: "+str(total1)+" runs.")
-        print("\n\n---------\n\n")
-        isOut = True
+print("Welcome to the Game of HandCricket.\nYou will play against the computer.")
+print("Toss!")
+print("Press 0 of heads, or 1 for tails. ")
+coin = input()
+
+if(coin == toss):
+    print("You won the toss. Select 0 to Bat, or 1 to Bowl. ")
+    choice = input()
+    if(choice == 0):
+        print("You choose to Bat first. ")
+        isBatting = True
     else:
-        print("You hit "+str(batsman)+" runs.")
-        total1+=batsman
-        print("Current Score: "+str(total1))
-        print("\n---------\n")
+        print("You choose to bowl first. ")
+        isBatting = False
+else:
+    if(toss == 0):
+        print("Computer choose to Bowl first. ")
+        isBatting = True
+    else:
+        print("Computer choose to Bat first. ")
+        isBatting = False
+        
+    
+
+
+def user_bats(  ):
+    print("You are batting: ")
+    while(isOut!=True):
+        print("Enter: ")
+        batsman = int(input())
+        bowler = bowl()
+    
+        if(batsman == bowler):
+            print("You are out!")
+            print("You scored: "+str(total1)+" runs.")
+            print("\n\n---------\n\n")
+            isOut = True
+        else:
+            print("You hit "+str(batsman)+" runs.")
+            total1+=batsman
+            print("Current Score: "+str(total1))
+            print("\n---------\n")
 
 print("You are bowling: ")
 isOut = False
