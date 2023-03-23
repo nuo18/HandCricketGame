34c9cda788b54219cf45b81dac1503e5591b732c
nuo18 <119784955+nuo18@users.noreply.github.com>
2023-03-23 22:11:42 +0700

    Re-changed everything

--- main.py
+++ main.py
@@ -1,74 +1,11 @@
-from randomize import *
+import randomize
 
-batsman = 0
-bowler = 0
+isBatting = True
 isOut = False
-isBatting = False
-total1 = 0
-total2 = 0
-choice = 0
+Total_First = 0
+Total_Second = 0
 
-print("Welcome to the Game of HandCricket.\nYou will play against the computer.")
-print("Toss!")
-print("Press 0 of heads, or 1 for tails. ")
-coin = input()
-
-if(coin == toss):
-    print("You won the toss. Select 0 to Bat, or 1 to Bowl. ")
-    choice = input()
-    if(choice == 0):
-        print("You choose to Bat first. ")
-        isBatting = True
-    else:
-        print("You choose to bowl first. ")
-        isBatting = False
-else:
-    if(toss == 0):
-        print("Computer choose to Bowl first. ")
-        isBatting = True
-    else:
-        print("Computer choose to Bat first. ")
-        isBatting = False
-        
-    
-#I dont knwo what to do
-
-def user_bats(  ):
-    print("You are batting: ")
-    while(isOut!=True):
-        print("Enter: ")
-        batsman = int(input())
-        bowler = bowl()
-    
-        if(batsman == bowler):
-            print("You are out!")
-            print("You scored: "+str(total1)+" runs.")
-            print("\n\n---------\n\n")
-            isOut = True
-        else:
-            print("You hit "+str(batsman)+" runs.")
-            total1+=batsman
-            print("Current Score: "+str(total1))
-            print("\n---------\n")
-
-print("You are bowling: ")
-isOut = False
-while(isOut!=True):
-    print("Enter: ")
-    bowler = int(input())
-    batsman = bat()
-    
-    if(total2 > total1):
-        print("You lost the game!")
-        print("Try again.")
-        isOut = True
-    else:
-        if(bowler == batsman):
-            print("You won the game!")
-            print("You won by "+str(total1-total2))
-            isOut = True
-        else:
-            print("Computer hit "+str(batsman)+" runs.")
-            total2+=batsman
-            print("More "+str(total1-total2)+" runs to win.")
-            print("\n---------\n")
+# Toss methods
+toss_win = randomize.toss()
+# Checking whether user is batting or bowling
+isBatting = randomize.choice(toss_win)
--- randomize.py
+++ randomize.py
@@ -1,10 +1,34 @@
 import random
 
 def toss():
-    return random.randint(0,1)
+    print("Press 1 for Heads and 2 for tails. ")
+    user_toss = int(input())
+    comp_gen = random.randint(1, 2)
+    if(user_toss == comp_gen):
+        print("You won the toss!")
+        return True
+    else:
+        print("You lost the toss!")
+        return False
 
-def bowl():
-    return random.randint(0,6)
+def choice(toss_win):
+    if(toss_win == True):
+        print("Press 1 to bat and 2 to Bowl.")
+        user_choice = int(input())
+        if(user_choice == 1):
+            print("You choose to Bat.")
+            return True
+        else:
+            print("You choose to Bowl.")
+            return False
+    else:
+        computer_choice = random.randint(1, 2)
+        if(computer_choice == 1):
+            print("Computer choose to Bat.")
+            print("You will Bowl now.")
+            return False
+        else:
+            print("Computer choose to Bowl.")
+            print("You will Bat now.")
+            return True
 
-def bat():
-    return random.randint(0,6)
