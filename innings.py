5b068a61c5783269f23bc8073660f4736fb2e6c1
nuo18 <119784955+nuo18@users.noreply.github.com>
2023-04-10 17:21:19 +0700

    FIxed and completed BattingFirst()

--- innings.py
+++ innings.py
@@ -9,7 +9,7 @@ def first_batting(isBatting):
     
     while(isBatting):
         pause()
-        print("-----")
+        space()
         print("Enter: ")
         user_cur = int(input()) # User inputs batting choice
         comp_bowl = random.randint(1,6) # Computes random bowl number from 1-6
@@ -19,16 +19,49 @@ def first_batting(isBatting):
             print("You scored a total of "+str(user_score)+" runs.")
             isBatting = False
         else:
-            print("Computer selected: "+str(comp_bowl))
+            print("Computer bowled: "+str(comp_bowl))
             user_score = user_score + user_cur # current runs are added to total
             print("Total Runs: "+str(user_score))
     
-    print("-----")
+    space()
     print("First inning total: " + str(user_score))
     print("Need to defend: " + str(user_score + 1))
-    print("-----")
+    space()
+    space()
     
     user_score = user_score+1
+    comp_score = 0
+    # checking whether the computer has crossed the user's score
+    while(comp_score <= user_score):
+        pause()
+        space()
+        print("Enter: ")
+        user_cur = int(input()) # User inputs bowling choice
+        comp_bowl = random.randint(1,6) # Computes random bat number from 1-6
+        pause()
+        if(user_cur == comp_bowl):
+            print("The computer is out! ")
+            print("It scored a total of "+str(comp_score)+" runs.")
+            print("You won by "+str(user_score-comp_score))
+            print("GGs")
+            break
+        elif(comp_score > user_score):
+            print("Computer batted: "+str(comp_bowl))
+            print("Total Runs: "+str(comp_score))
+            print("You lost! GGs")
+        else:
+            print("Computer batted: "+str(comp_bowl))
+            comp_score = comp_score + comp_bowl # current runs are added to total
+            print("Total Runs: "+str(comp_score))
+    space()
+    space()
+    print("Thanks for playing!")
+    
+            
+        
 
 def pause():
     time.sleep(1)
+
+def space():
+    print("---------")
