e35fccd392f331014bbff2cae7929e8dd2176f7e
nuo18 <119784955+nuo18@users.noreply.github.com>
2023-04-17 19:20:07 +0700

    CLI complete (some bugs) Need GUI now

--- innings.py
+++ innings.py
@@ -41,7 +41,7 @@ def first_batting(isBatting):
         pause()
         if(user_cur == comp_bowl):
             print("The computer is out! ")
-            print("It scored a total of "+str(comp_score)+" runs.")
+            print("The computer scored a total of "+str(comp_score)+" runs.")
             print("You won by "+str(user_score-comp_score))
             print("GGs")
             break
@@ -81,4 +81,50 @@ def second_batting(isBatting):
         comp_random = random.randint(1,6)
         pause()
         if(user_cur == comp_random):
-            print("")
+            print("The computer is out!")
+            print("The computer scored a total of: "+str(comp_score))
+            isBatting = True
+        else:
+            print("The Computer hit: "+str(comp_random))
+            comp_score = comp_score + comp_random # current runs are added to total
+            print("Total: "+str(comp_score))
+        
+    space()
+    print("First innings total: " + str(comp_score))
+    comp_score = comp_score + 1
+    print("Need to score: "+ str(comp_score)) # adds one to total because of first innings run
+    space()
+    space()
+    isBatting = True
+        
+    user_score = 0 # resets random number generator to start of loop for next user input
+    while(comp_score >= user_score):
+        pause()
+        space()
+        print("Enter:")
+        user_cur = int(input()) # input is converted to an int for comparison later on to see if user hit a bat or bowl
+        comp_random = random.randint(1,6) # resets random number generator for next user input to start of loop for next user input
+        pause()
+        if(user_cur == comp_random):
+            print("The computer threw a: "+str(comp_random))
+            print("You are out! ")
+            print("You scored a total of: "+str(user_score)) # user wins the game and scored a total of the user's runs
+            print("The computer won by "+str(comp_score - user_score)+" runs") # comp wins by the number of runs scored by user, minus the number of runs scored by user, wins the game
+            print("GGs")
+            break
+        else:
+            print("Computer bowled: "+str(comp_random))
+            user_score = user_score + user_cur
+            print("Total Runs: "+str(user_score))
+            print("More "+str(comp_score-user_score)+" needed to win")
+        
+        if(user_score > comp_score):
+            print("The Computer bowled: "+str(comp_random))
+            print("Total runs: "+str(user_score))
+            print("You won! GGs")
+            break
+    
+    space()
+    space()
+    print("Thanks for playing!")
+        
